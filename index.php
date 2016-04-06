38fd10fc45fd17e776a2ce56e7426c95ee803fa5
traicool <artemamrzv@gmail.com>
2016-04-06 14:29:32 +0300

    fignia

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Привет</h1>
+        <?php
+            echo "GIT";
+        ?>
+    </body>
+</html>
+
+
+
+
